7cc2ea46c9d996240fe40a81139596e8b97995dc
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-05-15 15:21:55 +0200

    feat(header): add header component

---
+++ src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/components/header/header.component";
+
+@Component({
+  selector: 'app-main-page',
+  imports: [HeaderComponent],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.scss'
+})
+export class MainPageComponent {
+
+}
