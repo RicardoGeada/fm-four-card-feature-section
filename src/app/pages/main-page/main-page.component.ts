e9cc24413f75cb8e48f0a00554cfe19ef8a34746
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-05-16 08:21:51 +0200

    feat(card): implement card component structure and relocate assets to public/

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
+import { CardComponent } from "../../shared/components/card/card.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CardComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
